f68a53c381123b1ce23cd8bd0704039532db5d8e
mike.pluquin <mike.pluquin@gmail.com>
2019-09-24 13:42:50 +0200

    init errorboundary

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Switch, withRouter, Redirect } from 'react-router-dom'
 
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary'
 import Layout from './components/Layout/Layout'
 import Home from './containers/Home/Home'
 
 const app = props => {
   return (
-    <Switch>
-      <Layout exact path="/" component={Home} />
-      <Redirect to="/" />
-    </Switch>
+    <ErrorBoundary>
+      <Switch>
+        <Layout exact path="/" component={Home} />
+        <Redirect to="/" />
+      </Switch>
+    </ErrorBoundary>
   )
 }
 
---
+++ src/components/ErrorBoundary/ErrorBoundary.js
@@ -0,0 +1,33 @@
+// https://fr.reactjs.org/docs/error-boundaries.html
+import React, { Component } from 'react'
+
+class ErrorBoundary extends Component {
+  state = {
+    hasError: false
+  }
+
+  static getDerivedStateFromError(error) {
+    // Mettez à jour l'état, de façon à montrer l'UI de repli au prochain rendu.
+    return {
+      hasError: true
+    }
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // Vous pouvez aussi enregistrer l'erreur au sein d'un service de rapport.
+    // logErrorToMyService(error, errorInfo)
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // Vous pouvez afficher n'importe quelle UI de repli.
+      return (
+        <p>Error !</p>
+      )
+    }
+
+    return this.props.children
+  }
+}
+
+export default ErrorBoundary
