7e51187a3ede33a28dadfddaaadfd4b3f645cc39
mike.pluquin <mike.pluquin@gmail.com>
2019-09-24 18:34:11 +0200

    update error layout

--- src/components/Layout/ErrorLayout/ErrorLayout.js
+++ src/components/Layout/ErrorLayout/ErrorLayout.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
+import Icon from '../../UI/Icon/Icon'
+
 const errorLayout = (props) => {
   return (
     <div id="layout" className="layout_error">
       <main>
         <div className="h-100 d-flex align-items-center justify-content-center text-light">
           <h1>
-            <i className="fa fa-exclamation-triangle" />
-            &nbsp;
             Oops, something went wrong !
-            </h1>
+          </h1>
         </div>
       </main>
     </div >
